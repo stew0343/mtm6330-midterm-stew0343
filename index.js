ad53f52b115db190c9bebf66c7de8eea04d896f8
Michelle Stewart <stew0343@algonquinlive.com>
2018-10-16 23:10:17 -0400

    Update gallery and couple pages

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+////////////////////////////////
+// COUNTDOWN
+
 var endDate = Date.parse('oct 12 2019 23:59:59');
 var today = Date.parse(new Date());
 var difference = Math.floor((endDate - today) / (24 * 60 * 60 * 1000));
@@ -9,3 +12,32 @@ if (difference === 1) {
 } else {
     document.getElementById('countdown').innerHTML = difference + ' days to go!';
 }
+
+/////////////////////////////////
+// GALLERY
+
+var $pictures = document.getElementById('pictures');
+var $image = document.getElementById('image');
+var $overlay = document.getElementById('overlay');
+
+$pictures.addEventListener('click', function(e) {
+    if (e.target.classList.contains('gallery')) {
+        //showPicture()
+        console.log(e.target.id);
+        $image.innerHTML = `<img src="images/gallery/` + e.target.id + `.jpg">`;
+        //window.scrollTo(0,0);
+        showOverlay();
+    }
+})
+
+function showPicture() {
+    $image.innerHTML = `<img src="images/gallery/` + `andy.png` + `">`;
+}
+
+function showOverlay() {
+    $overlay.classList.remove('hidden');
+}
+
+$image.addEventListener('click', function(e) {
+    $overlay.classList.add('hidden');
+})
